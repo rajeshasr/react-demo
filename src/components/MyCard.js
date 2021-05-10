ebd695b8e3e0ba4b9c860d70eec25b385606594b
rajeshasr <rajeshasr@gmail.com>
2021-05-10 13:00:56 +0530

    made changes

---
+++ src/components/MyCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Card } from 'antd';
+
+const MyCard = () => {
+  return (
+  <>
+  <h1>Card</h1>
+  <Card title="Card title" hoverable>
+      <p>Card content</p></Card>
+
+  </>
+  );
+};
+export default MyCard;
